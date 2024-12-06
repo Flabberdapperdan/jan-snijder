10781a2209aa62c61828eb3f9f9e9357e1d9865c
Matthijs G. Snijder <snijdermatthijs@gmail.com>
2024-12-06 15:32:24 +0100

    add exhibits route and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { ExhibitsComponent } from './exhibits/exhibits.component';
 
 export const routes: Routes = [
-  {    path: '',    title: 'Home',    component: HomeComponent,  },
-  {    path: 'contact',    title: 'Contact',    component: ContactComponent,  },
+  { path: '', title: 'Home', component: HomeComponent },
+  { path: 'exposities', title: 'Exposities', component: ExhibitsComponent },
+  { path: 'contact', title: 'Contact', component: ContactComponent },
 ];
---
+++ src/app/exhibits/exhibits.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exhibits',
+  standalone: true,
+  imports: [],
+  templateUrl: './exhibits.component.html',
+  styleUrl: './exhibits.component.css'
+})
+export class ExhibitsComponent {
+
+}
