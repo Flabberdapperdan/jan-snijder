ed02b01915847e5133b62de89ef1c253c5ab3d3f
Matthijs G. Snijder <snijdermatthijs@gmail.com>
2025-01-10 16:59:57 +0100

    setup first styling carousel and add icons

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
   imports: [],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.css'
+  styleUrl: './carousel.component.css',
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
+  images: Array<Image> = [];
+  activeImageIndex: number = 0
 
+  ngOnInit(): void {
+    this.images = [
+      {
+        index: 0,
+        src: 'https://cdn.nos.nl/image/2024/03/29/1067142/1024x576a.jpg',
+      },
+      {
+        index: 1,
+        src: 'https://cdn.nos.nl/image/2024/03/29/1067142/1024x576a.jpg',
+      },
+      {
+        index: 2,
+        src: 'https://cdn.nos.nl/image/2024/03/29/1067142/1024x576a.jpg',
+      },
+    ];
+  }
+}
+
+interface Image {
+  index: number;
+  src: string;
 }
