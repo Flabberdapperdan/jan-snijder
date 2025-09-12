b132a5fde071235f65a299d3d508ec5227fe9750
Matthijs G. Snijder <snijdermatthijs@gmail.com>
2025-09-12 14:54:45 +0200

    add sanity service

---
+++ src/app/servicee/sanity.service.ts
@@ -0,0 +1,39 @@
+// src/app/service/sanity.service.ts
+
+import { Injectable } from '@angular/core';
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SanityService {
+  constructor() {}
+
+  sanityClientCredentials = {
+    option: create({
+      projectId: 'YOUR_PROJECT_ID',
+      dataset: 'production',
+    }),
+  };
+
+  urlFor = (source: any) =>
+    imageUrlBuilder(this.sanityClientCredentials.option).image(source);
+
+  async getPaintings(): Promise<Painting[]> {
+    return await this.sanityClientCredentials.option.fetch(
+      `*[_type == "movie"]{
+        _id,
+        image,
+        title,
+      }`
+    );
+  }
+}
+
+// Types
+interface Painting {
+  _id: string;
+  image: string;
+  title: string;
+}
