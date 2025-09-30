eafbef6e06b3d2c0eb6dfebd2ef87e776e357970
Matthijs G. Snijder <snijdermatthijs@gmail.com>
2025-09-30 19:33:05 +0200

    first try at making home content related

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from './services/home.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  title: string = '';
+  subtitle: string = '';
+
+  constructor(private service: HomeService) { }
+
+  ngOnInit(): void {
+    this.getContent();
+  }
+
+  getContent(): void {
+    console.log('Fetching home content...');
+    this.service.getContent().then((data) => {
+      this.title = data[0].title;
+      this.subtitle = data[0].subtitle;
+      console.log('Home content fetched:', data);
+      console.log('Title:', this.title);
+      console.log('Subtitle:', this.subtitle);
+
+    }).catch((error) => {
+      console.error('Error fetching home content:', error);
+    });
+  }
+}
