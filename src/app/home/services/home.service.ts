b88266fb80c111eb712ee0dc438c70817b78ad4d
flabberdapperdan <snijdermatthijs@gmail.com>
2025-10-10 12:56:18 +0200

    add homepage content

--- src/app/home/services/home.service.ts
+++ src/app/home/services/home.service.ts
@@ -1,20 +1,20 @@
-import { Injectable } from '@angular/core';
-import { Home } from '../home.types';
-import { client } from '../../../../sanity/client';
+import { Injectable } from '@angular/core'
+import { Home } from '../home.types'
+import { client } from '../../../../sanity/client'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomeService {
   private HOME_CONTENT = `
    *[_type == "home"]{
       _id,
       title,
-      title,
+      subtitle,
       }
-  `;
+  `
 
   async getContent(): Promise<Home[]> {
-    return await client.fetch<Home[]>(this.HOME_CONTENT, {}, {}); // Do we need options here?
+    return await client.fetch<Home[]>(this.HOME_CONTENT, {}, {}) // Do we need options here?
   }
 }
