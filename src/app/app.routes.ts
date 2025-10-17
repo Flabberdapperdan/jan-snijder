56521943a800e9f939efabf5766f67e7f584e20c
flabberdapperdan <snijdermatthijs@gmail.com>
2025-10-17 16:23:26 +0200

    remove contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { ExhibitsComponent } from './exhibits/exhibits.component';
 import { CarouselComponent } from './carousel/carousel.component';
-import { SanityClient } from '@sanity/client';
 
 export const routes: Routes = [
   { path: '', title: 'Home', component: HomeComponent },
   { path: 'exposities', title: 'Exposities', component: ExhibitsComponent },
-  { path: 'contact', title: 'Contact', component: ContactComponent },
   { path: 'werken', title: 'Werken', component: CarouselComponent },
 ];
