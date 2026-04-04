682b53b7667c953ad635c6e5d96458f6f1dab7fd
flabberdapperdan <snijdermatthijs@gmail.com>
2026-04-04 17:16:59 +0200

    fix imports

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
